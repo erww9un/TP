2ff6969daebf5a6b960428e790d3078fd1476046
erww9un <erwannrocher@gmail.com>
2025-11-14 10:30:34 +0100

    Add files via upload

--- tp14.js
+++ tp14.js
@@ -70,8 +70,9 @@ function addLigne() {
     //Récupérer la reference du tableau
     var name = document.getElementById("nom").value;
     var age = document.getElementById("age").value;
+    var prenom = document.getElementById("prenom").value;
 
-    if (name == "" || age == "") {
+    if (name == "" || age == "" || prenom == "") {
         alert("un des deux éléments n'est pas complet ou valide");
     }
     else if (age < 10 || age > 20) {
@@ -80,10 +81,14 @@ function addLigne() {
     else {
         var table = document.getElementById("myTable");
         var newRow = table.insertRow(-1);
+
         var cell1 = newRow.insertCell(0);
         var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+
         cell1.innerHTML = name;
         cell2.innerHTML = age;
+        cell3.innerHTML = prenom;
     }
 
 }
